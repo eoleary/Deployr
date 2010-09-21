ee4e215881a29a1c0342f9c980094701417e19a0
Christopher Bradford <bradfordcp@gmail.com>
2010-09-21 08:27:24 -0400

    Traversing FTP system

--- app/models/ftp_server.rb
+++ app/models/ftp_server.rb
@@ -9,8 +9,29 @@ class FtpServer < Server
     
     # Iterate over the added, removed, and modified arrays
     ftp = Net::FTP.new self.hostname
+    
+    # Login with the supplied credentials
     ftp.login self.username, self.password
+    
+    # We assume the initial path already exists
     ftp.chdir commit.push.path
+    
+    # Now perform work
+    commit.added.each do |path|
+      # Traverse the path building directories as necessary
+      # TODO Verify it is a directory
+      path.split("/").each do |part|
+        #ftp.mkdir(part) if ftp.dir.find_all {|obj| obj.match(/.*#{part}$/}
+        ftp.mkdir(part) unless ftp.nlst.include? part
+        ftp.chdir(part)
+      end
+      
+      # Checkout the file to a temporary location
+      
+    end
+    
+    # Close the connection
+    ftp.close
   end
   handle_asynchronously :push_commit
 end
