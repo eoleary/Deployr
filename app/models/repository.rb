35099131d1a3193c301d0c42f11bb2658e005665
Christopher Bradford <bradfordcp@gmail.com>
2010-09-24 02:05:45 -0400

    Populated the home screen and added basic stylesheets

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,6 @@
 class Repository < ActiveRecord::Base
   # Associations
   belongs_to :site
-  has_many :gh_pushes
+  has_many :gh_pushes, :dependent => :destroy
   belongs_to :owner, :class_name => "GhUser"
 end
